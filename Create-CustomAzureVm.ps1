7135fb224e9343f4663cefe0e611ef2f849f5f45
kkuflewska <klaudyna.kuflewska@accenture.com>
2019-04-22 19:11:29 +0200

    fixing structure

--- Create-CustomAzureVm.ps1
+++ Create-CustomAzureVm.ps1
@@ -5,7 +5,7 @@ Param(
 
     [Parameter(Mandatory=$true)]
     [String] 
-    $resourceGroupName,
+    $ResourceGroupName,
 
     [Parameter(Mandatory=$true)]
     [String] 
@@ -13,38 +13,58 @@ Param(
 )
 
 # Connect to Azure with an authenticated account
-Connect-AzureRmAccount 
+# Connect-AzureRmAccount 
 
-# Create resource group
-New-AzureRmResourceGroup `
-    -Name $resourceGroupName `
-    -Location $location 
+# Check if the resource group already exists
+$exists = Get-AzureRmResourceGroup `
+    -ResourceGroupName $ResourceGroupName 
+if ($exists) {
+    Write-Host "Resource group already exists"
+} else {
+    Write-Host "Create new resource group"
+    
+    New-AzureRmResourceGroup `
+        -Name $ResourceGroupName `
+        -Location $location 
+}
 
-# Set the username and password needed for the administrator account 
-$cred = Get-Credential
+# Check if the VM already exists
+try {
+    $vmexists = Get-AzureRmVm `
+    -ResourceGroupName $ResourceGroupName `
+    -VMName $VMName `
+    -ErrorAction SilentlyContinue
+} catch {
+}
 
-# Create the VM
-New-AzureRmVM `
-    -ResourceGroupName $resourceGroupName `
-    -Name $VMName `
-    -Location $location `
-    -VirtualNetworkName "myVnet" `
-    -SubnetName "mySubnet" `
-    -SecurityGroupName "myNSG" `
-    -PublicIpAddressName "myPublicIpAddress" `
-    -OpenPorts 80,3389 `
-    -Credential $cred 
+if (-not $vmexists) {
+    # Set the username and password needed for the administrator account 
+    $cred = Get-Credential
 
-# Resize a VM
-$vm = Get-AzureRmVM `
-    -ResourceGroupName $resourceGroupName `
-    -VMName $VMName 
-$vm.HardwareProfile.VmSize = "Standard_B1s"
-Update-AzureRmVM `
-    -VM $vm `
-    -ResourceGroupName $resourceGroupName
+    # Create the VM
+    New-AzureRmVM `
+     -ResourceGroupName $ResourceGroupName `
+     -Name $VMName `
+     -Location $location `
+     -VirtualNetworkName "myVnet" `
+     -SubnetName "mySubnet" `
+     -SecurityGroupName "myNSG" `
+     -PublicIpAddressName "myPublicIpAddress$VMName" ` 
+     -Credential $cred 
 
-# Stop a VM
-Stop-AzureRmVM `
-    -ResourceGroupName $resourceGroupName `
-    -Name $VMName -Force
+    # Resize a VM
+    $vm = Get-AzureRmVM `
+     -ResourceGroupName $ResourceGroupName `
+     -VMName $VMName 
+    $vm.HardwareProfile.VmSize = "Standard_B1s"
+    Update-AzureRmVM `
+     -VM $vm `
+     -ResourceGroupName $ResourceGroupName
+
+    # Stop a VM
+    Stop-AzureRmVM `
+     -ResourceGroupName $ResourceGroupName `
+     -Name $VMName -Force
+} else {
+    Write-Error "VM already exists"
+}
