fb6869ce0acdb67a0604065a670ddb7114500d42
kkuflewska <klaudyna.kuflewska@accenture.com>
2019-05-06 09:57:25 +0200

    add Key Vault authentication

--- templates/upload-template.ps1
+++
@@ -1,6 +0,0 @@
-$resourceGroupName = "anotherResGroup"
-$location = "West Europe"
-
-#New-AzResourceGroup -Name $resourceGroupName -Location $location
-New-AzureRmResourceGroupDeployment `
-    -TemplateFile .\Create-ResourceGroup.json
